4a4b18ec3dc5382d5c03ba84e539d245029b61a9
Milton Figueredo Miles <gratno@gmail.com>
2022-11-21 20:05:35 -0500

    Correción moviendo carpetas js

--- servicios/productos-servicio.js
+++ servicios/productos-servicio.js
@@ -1,3 +1,7 @@
 //GET 
 
-const listaProdutos = () => fetch("http://localhost:3000/producto").then()
+const listaProductos = () => fetch("http://localhost:3000/producto").then(respuesta => respuesta.json());
+
+export const  productoService = {
+    listaProductos,
+}
