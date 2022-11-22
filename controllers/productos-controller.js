a45a6764a707b3d44c75daef800f34662b5a80b5
Milton Figueredo Miles <gratno@gmail.com>
2022-11-21 20:28:55 -0500

    Actualizando Productos página

--- controllers/productos-controller.js
+++ controllers/productos-controller.js
@@ -1,11 +1,13 @@
-import {productoService} from "../servicios/productos-servicio.js";
+import { productoService } from "../servicios/productos-servicio.js";
 
-const nuevoProducto = (name, price, imageUrl) => {
+const nuevoProducto = (name, price, imageUrl, alt, section) => {
     const card = document.createElement("div");
     const contenido = `
     <div>
     <img src="${imageUrl}" alt="">
+        <h2>${alt}</h2>    
         <h3>${name}<h3>
+        <h3>${section}</h3>
         <p>${price}</p>
     </div> `
 
@@ -20,7 +22,7 @@ const render = async () => {
     try {
         const listaProductos = await productoService.listaProductos()
         listaProductos.forEach(elemento => {
-            producto.appendChild(nuevoProducto(elemento.name, elemento.price, elemento.imageUrl))
+            producto.appendChild(nuevoProducto(elemento.name, elemento.price, elemento.imageUrl, elemento.alt, elemento.section))
         });
     }
     catch (erro) {
